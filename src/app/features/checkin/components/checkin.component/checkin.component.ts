52ab844d58d10ef697d7d26886f0dec9046fbf36
daniel <daniel.eng.dev@gmail.com>
2026-01-16 23:59:17 -0300

    feat: add structure

---
+++ src/app/features/checkin/components/checkin.component/checkin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkin.component',
+  imports: [],
+  templateUrl: './checkin.component.html',
+  styleUrl: './checkin.component.scss',
+})
+export class CheckinComponent {
+
+}
